7d637450ba3c9bd6827b3e06f4525da23535242b
Shuhei Akutagawa <54097100+akutagw@users.noreply.github.com>
2019-08-28 14:40:48 +0900

    Refactor raml-to-ts.js

--- raml-to-ts.js
+++ raml-to-ts.js
@@ -10,61 +10,76 @@ const { json2ts } = require("json-ts");
 const LANG = "ja";
 const CHATWORK_URL = "https://api.chatwork.com/v2";
 
-const ramlStr = fs.readFileSync(
-  path.join(__dirname, "api", "RAML", `api-${LANG}.raml`),
-  { encoding: "utf8" }
-);
-
-const ramlData = yaml.safeLoad(prettier.format(ramlStr, { parser: "yaml" }));
-
-const generateInterfaceName = (method, uri) => {
-  return pascalCase(
-    [method, uri.replace(/{/g, "_with_").replace(/}/g, "")].join("_")
+(function main() {
+  const ramlStr = fs.readFileSync(
+    path.join(__dirname, "api", "RAML", `api-${LANG}.raml`),
+    { encoding: "utf8" }
   );
-};
 
-const getQueryParameters = (queryParameters = {}) => {
-  return Object.keys(queryParameters).map(name => ({
-    ...queryParameters[name],
-    name
-  }));
-};
+  const ramlData = yaml.safeLoad(prettier.format(ramlStr, { parser: "yaml" }));
 
-const getResponses = (responses = {}) => {
-  if (!responses["200"]) return;
-  const example = responses["200"]["body"]["application/json"]["example"];
-  return example ? example.replace(/^\|/, "") : undefined;
-};
+  const data = parseApi(ramlData);
+  // console.log(data);
 
-const parseMethod = (method, api, uri) => {
-  const { description, queryParameters, responses, is } = api;
-  const name = generateInterfaceName(method, uri);
-  const funcName = camelCase(name);
-  const funcParams = [];
-  if (uri.includes("{room_id}")) funcParams.push("room_id");
-  if (uri.includes("{message_id}")) funcParams.push("message_id");
-  if (uri.includes("{task_id}")) funcParams.push("task_id");
-  if (uri.includes("{file_id}")) funcParams.push("file_id");
-  if (uri.includes("{request_id}")) funcParams.push("request_id");
-  const funcParamsWithTypes = funcParams.map(p => p + ": string");
+  const responseInterfaces = renderResponsesInterfaces(data);
 
-  const funcParam = funcName + "Param";
-  const funcParamWithTypes = funcName + "Param: " + name + "Param";
+  const paramInterfaces = renderParamInterfaces(data);
 
-  const params = getQueryParameters(queryParameters);
-  params.forEach(param => {
-    if (param.type === "integer") param.type = "number";
-    if (param.enum) param.type = `'${param.enum.join(`'|'`)}'`;
-    if (param.displayName) {
-      param.displayName = param.displayName.replace(/\n/g, "");
-    }
+  const apiClass = renderApiClass(data);
+
+  const importData = `
+import axios from 'axios';
+`;
+
+  const tsData =
+    importData +
+    apiClass +
+    addExport(paramInterfaces) +
+    addExport(responseInterfaces);
+
+  const prettifiedTsData = prettier.format(tsData, {
+    parser: "typescript"
   });
+
+  console.log(prettifiedTsData);
+
+  fs.writeFileSync(path.join(__dirname, "src", "api.ts"), prettifiedTsData);
+})();
+
+function parseApi(ramlData, prefix = "") {
+  return Object.keys(ramlData)
+    .filter(key => key.match(/^\//))
+    .map(uri => {
+      const api = ramlData[uri];
+      return [
+        ...parseApi(api, prefix + uri),
+        ...parseEndpoint(api, prefix + uri)
+      ];
+    })
+    .reduce((pre, cur) => [...pre, ...cur], []);
+}
+
+function parseEndpoint(api, uri) {
+  return Object.keys(api)
+    .filter(method => method.match(/GET|POST|PUT|DELETE/))
+    .map(method => parseMethod(method, api[method], uri));
+}
+
+function parseMethod(method, api, uri) {
+  const { description, queryParameters, responses } = api;
+  const ifName = getInterfaceName(method, uri);
+  const funcName = camelCase(ifName);
+  const funcParams = getFuncParams(uri);
+  const funcParamsWithTypes = funcParams.map(param => param + ": string");
+  const funcParam = funcName + "Param";
+  const funcParamWithTypes = funcParam + ": " + ifName + "Param";
+  const params = getQueryParameters(queryParameters);
 
   const res = getResponses(responses);
 
   return {
     method,
-    name,
+    ifName,
     funcName,
     funcParams,
     funcParamsWithTypes,
@@ -78,46 +93,55 @@ const parseMethod = (method, api, uri) => {
       ? description.replace(/^\|\s+/, "").replace(/\n/g, "")
       : ""
   };
-};
+}
 
-const parseEndpoint = (api, uri) => {
-  return Object.keys(api)
-    .filter(method => method.match(/GET|POST|PUT|DELETE/))
-    .map(method => parseMethod(method, api[method], uri));
-};
-
-const parseApi = (ramlData, prefix = "") => {
-  return Object.keys(ramlData)
-    .filter(key => key.match(/^\//))
-    .map(uri => {
-      const api = ramlData[uri];
-      return [
-        ...parseApi(api, prefix + uri),
-        ...parseEndpoint(api, prefix + uri)
-      ];
-    })
-    .reduce((pre, cur) => [...pre, ...cur], []);
-};
-
-const data = parseApi(ramlData);
-// console.log(data);
-
-const responseInterfaces = data
-  .filter(d => d.res)
-  .map(d =>
-    json2ts(d.res, {
-      rootName: d.name + "Response",
-      prefix: ""
-    })
+function getInterfaceName(method, uri) {
+  return pascalCase(
+    [method, uri.replace(/{/g, "_with_").replace(/}/g, "")].join("_")
   );
+}
 
-const paramInterfaces = ejs.render(
-  `
+function getQueryParameters(queryParameters = {}) {
+  const params = Object.keys(queryParameters).map(name => ({
+    ...queryParameters[name],
+    name
+  }));
+
+  params.forEach(param => {
+    if (param.type === "integer") param.type = "number";
+    if (param.enum) param.type = `'${param.enum.join(`'|'`)}'`;
+    if (param.displayName) {
+      param.displayName = param.displayName.replace(/\n/g, "");
+    }
+  });
+
+  return params;
+}
+
+function getFuncParams(uri) {
+  const funcParams = [];
+  if (uri.includes("{room_id}")) funcParams.push("room_id");
+  if (uri.includes("{message_id}")) funcParams.push("message_id");
+  if (uri.includes("{task_id}")) funcParams.push("task_id");
+  if (uri.includes("{file_id}")) funcParams.push("file_id");
+  if (uri.includes("{request_id}")) funcParams.push("request_id");
+  return funcParams;
+}
+
+function getResponses(responses = {}) {
+  if (!responses["200"]) return;
+  const example = responses["200"]["body"]["application/json"]["example"];
+  return example ? example.replace(/^\|/, "") : undefined;
+}
+
+function renderParamInterfaces(data) {
+  return ejs.render(
+    `
 <% data.forEach(d => { %>
 /**
  * <%- d.description %>
  */
-interface <%- d.name %>Param {
+interface <%- d.ifName %>Param {
   <% d.params.forEach(param => { %>
   /** <%- param.displayName %> */
   <%- param.name %>: <%- param.type %>
@@ -125,51 +149,57 @@ interface <%- d.name %>Param {
 }
 <% }); %>
   `,
-  { data: data.filter(d => d.params) }
-);
+    { data: data.filter(d => d.params) }
+  );
+}
 
-const apiFunctions = ejs.render(
-  `
+function renderResponsesInterfaces(data) {
+  return data
+    .filter(d => d.res)
+    .map(d =>
+      json2ts(d.res, {
+        rootName: d.ifName + "Response",
+        prefix: ""
+      })
+    )
+    .join("\n");
+}
+
+function renderApiClass(data) {
+  return ejs.render(
+    `
 export default class ChatworkApi {
 
   constructor(private api_token: string){}
 
 
   <% data.forEach(d => { %>
+  <%
+  let params = [...d.funcParamsWithTypes, d.funcParamWithTypes].join(', ');
+  if(!d.params || d.params.length === 0) {
+    params += ' = {}';
+  }
+  const url = '${CHATWORK_URL}' + d.uri.replace(/{/g, '\${');
+  const method = d.method.toLowerCase();
+  %>
   /**
    * <%- d.description %>
    */
-  async <%- d.funcName %>(<%- [...d.funcParamsWithTypes, d.funcParamWithTypes].join(', '); %><% if(!d.params || d.params.length === 0) { %> = {}<% } %>) {
-    <% if(d.method === 'GET' || d.method === 'DELETE') { %>
-    const {data} = await axios.<%- d.method.toLowerCase() %>(\`${CHATWORK_URL}<%- d.uri.replace(/{/g, '\${') %>\`, { params: <%- d.funcParam %>, headers: { 'X-ChatWorkToken': this.api_token }});
-    <% } else if(d.method === 'POST' || d.method === 'PUT') { %>
-    const {data} = await axios.<%- d.method.toLowerCase() %>(\`${CHATWORK_URL}<%- d.uri.replace(/{/g, '\${') %>\`, <%- d.funcParam %>, { headers: { 'X-ChatWorkToken': this.api_token }});
+  async <%- d.funcName %>(<%- params %>) {
+    <% if(method === 'get' || method === 'delete') { %>
+    const { data } = await axios.<%- method %>(\`<%- url %>\`, { params: <%- d.funcParam %>, headers: { 'X-ChatWorkToken': this.api_token }});
+    <% } else if(method === 'post' || method === 'put') { %>
+    const { data } = await axios.<%- method %>(\`<%- url %>\`, <%- d.funcParam %>, { headers: { 'X-ChatWorkToken': this.api_token }});
     <% } %>
     return data;
   }
   <% }); %>
 }
 `,
-  { data }
-);
+    { data }
+  );
+}
 
-const importData = `
-import axios from 'axios';
-`;
-
-const tsData = (
-  importData +
-  apiFunctions +
-  paramInterfaces +
-  responseInterfaces.join("\n")
-)
-  .replace(/interface/g, "export interface")
-  .replace(/function/g, "export function");
-
-const prettifiedTsData = prettier.format(tsData, {
-  parser: "typescript"
-});
-
-// console.log(prettifiedTsData);
-
-fs.writeFileSync(path.join(__dirname, "src", "api.ts"), prettifiedTsData);
+function addExport(tsData) {
+  return tsData.replace(/interface/g, "export interface");
+}
