56957337823e4cd84d2a43cd94f1d4e19691c3ff
Shuhei Akutagawa <54097100+akutagw@users.noreply.github.com>
2019-08-30 10:40:16 +0900

    Run prettier before build

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,48 +1,48 @@
-import ChatworkApi from ".";
-import * as fs from "fs";
-import * as path from "path";
+import ChatworkApi from '.';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const API_TOKEN_PATH = path.resolve(__dirname, "../API_TOKEN");
+const API_TOKEN_PATH = path.resolve(__dirname, '../API_TOKEN');
 
 const API_TOKEN = getApiToken();
 
-test("API Token", () => {
+test('API Token', () => {
   expect(API_TOKEN).toMatch(/^[a-z0-9]+$/);
 });
 
-test("API Connection Test (GET,POST,PUT,DELETE)", async () => {
+test('API Connection Test (GET,POST,PUT,DELETE)', async () => {
   const api = new ChatworkApi(API_TOKEN);
 
   const me = await api.getMe();
-  expect(me).toHaveProperty("room_id");
+  expect(me).toHaveProperty('room_id');
   const { room_id } = me;
 
   const message = await api.postRoomMessage(room_id, {
-    body: "[info][title]chatwork-api-client[/title]test[/info]"
+    body: '[info][title]chatwork-api-client[/title]test[/info]',
   });
-  expect(message).toHaveProperty("message_id");
+  expect(message).toHaveProperty('message_id');
   const { message_id } = message;
 
   const putResult = await api.putRoomMessage(room_id, message_id, {
-    body: "[info][title]chatwork-api-client[/title]test(edited)[/info]"
+    body: '[info][title]chatwork-api-client[/title]test(edited)[/info]',
   });
-  expect(putResult).toHaveProperty("message_id");
+  expect(putResult).toHaveProperty('message_id');
 
   const deleteResult = await api.deleteRoomMessage(room_id, message_id);
-  expect(deleteResult).toHaveProperty("message_id");
+  expect(deleteResult).toHaveProperty('message_id');
 });
 
 function getApiToken() {
   if (!fs.existsSync(API_TOKEN_PATH)) {
     throw new Error(
-      ['The file "API_TOKEN" is not found.', API_TOKEN_PATH].join("\n")
+      ['The file "API_TOKEN" is not found.', API_TOKEN_PATH].join('\n'),
     );
   }
   return fs
     .readFileSync(API_TOKEN_PATH, {
-      encoding: "utf8"
+      encoding: 'utf8',
     })
-    .split("\n")
-    .filter(line => line && !line.startsWith("#"))
-    .join("");
+    .split('\n')
+    .filter(line => line && !line.startsWith('#'))
+    .join('');
 }
