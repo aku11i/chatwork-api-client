308193be173339feb499831da6743cf5d7b37001
Shuhei Akutagawa <readonly@akutagawa.dev>
2020-06-23 23:20:53 +0900

    Fix CLI test: use ts-node to run command

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -6,38 +6,57 @@ const ROOT = resolve(__dirname, "..");
 
 process.chdir(ROOT);
 
-test("API Connection Test[CLI] (GET,POST,PUT,DELETE)", async () => {
-  const meJson = execSync(
-    "yarn --silent start get-me --format json",
-  ).toString();
-  const me = JSON.parse(meJson);
+function exec(...args: string[]) {
+  const command = [
+    "npx ts-node src/cli.ts",
+    ...args.map((arg) => `"${arg}"`),
+  ].join(" ");
 
+  const result = execSync(command).toString();
+  return JSON.parse(result);
+}
+
+test("API Connection Test[CLI] (GET,POST,PUT,DELETE)", async () => {
+  const me = exec("get-me", "--format", "json");
   expect(me).toHaveProperty("room_id");
   const { room_id } = me;
 
-  const messageJson = execSync(
-    `yarn --silent start post-room-message --format json --body "[info][title]chatwork-api-client[CLI][/title]test[/info]" "${room_id}"`,
-  ).toString();
-  const message = JSON.parse(messageJson);
+  const message = exec(
+    "post-room-message",
+    "--format",
+    "json",
+    "--body",
+    "[info][title]chatwork-api-client[CLI][/title]test[/info]",
+    room_id,
+  );
 
   expect(message).toHaveProperty("message_id");
   const { message_id } = message;
 
   await sleep();
 
-  const deleteResultJson = execSync(
-    `yarn --silent start delete-room-message --format json "${room_id}" "${message_id}"`,
-  ).toString();
-  const deleteResult = JSON.parse(deleteResultJson);
+  const deleteResult = exec(
+    "delete-room-message",
+    "--format",
+    "json",
+    room_id,
+    message_id,
+  );
 
   expect(deleteResult).toHaveProperty("message_id");
 
   const filePath = join(__dirname, "test.txt");
 
-  const postFileResultJson = execSync(
-    `yarn --silent start post-room-file --format json --file_name "testfile.cli.txt" --file "${filePath}" "${room_id}"`,
-  ).toString();
-  const postFileResult = JSON.parse(postFileResultJson);
+  const postFileResult = exec(
+    "post-room-file",
+    "--format",
+    "json",
+    "--file_name",
+    "testfile.cli.txt",
+    "--file",
+    filePath,
+    room_id,
+  );
 
   expect(postFileResult).toHaveProperty("file_id");
 });
